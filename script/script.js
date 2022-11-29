1b9bd0ca49afb540f92b24ea5e158bfed4c89883
francescotrudu <francescotrudu84@gmail.com>
2022-11-29 17:54:28 +0100

    Add input html

--- script/script.js
+++ script/script.js
@@ -3,26 +3,41 @@ Chiedere all’utente di inserire una parola:
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-function isPalindroma(word) {
-    // Misuro la lunghezza della parola inserita
-    const lunghezza = word.length;
+const buttonPalindrome = document.getElementById("palindrome-btn");
+buttonPalindrome.addEventListener("click", function(){
 
-    // Divido la parola a metà
-    let check = true;
-    for (let i = 0; i < lunghezza / 2; i++) {
-        //variabile di appoggio true
-        // controlla se il primo e ultimo carattere son diversi
-        if (word[i] !== word[lunghezza - 1 - i]) {
-            //se il primo e ultimo carattere son diversi return false
-            check = false;
+    
+    function isPalindroma(word) {
+        // Misuro la lunghezza della parola inserita
+        const lunghezza = word.length;
+        
+        // Divido la parola a metà
+        let check = true;
+        for (let i = 0; i < lunghezza / 2; i++) {
+            //variabile di appoggio true
+            // controlla se il primo e ultimo carattere son diversi
+            if (word[i] !== word[lunghezza - 1 - i]) {
+                //se il primo e ultimo carattere son diversi return false
+                check = false;
+            }
         }
+        // se son uguali return true
+        return check;
     }
-    // se son uguali return true
-    return check;
-}
+    
+    // const userWord = prompt(`Inserisci una parola`);
+    const userWord = document.getElementById("userWord").value;
+    const outputResult = document.getElementById("output");
 
-const userWord = prompt(`Inserisci una parola`)
+    const result = isPalindroma(userWord);
+    // console.log(result);
 
-const result = isPalindroma(userWord);
+    if (result) {
+        outputResult.innerHTML = 'Questa parola è palindroma';
+        // console.log("Palindroma");
+    } else {
+        outputResult.innerHTML = 'Questa parola non è palindroma';
+        // console.log("Non palindroma");
+    }
 
-console.log(result);
+})
